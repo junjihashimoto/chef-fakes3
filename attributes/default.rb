8f64800a7b82b8a821717b249bba0bf11ed75ff1
Junji Hashimoto <junji.hashimoto@gree.net>
2014-05-05 19:00:07 +0900

    Added /etc/init.d/fakes3 template

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default["fake-s3"]["s3port"] = 80
+default["fake-s3"]["s3root"] = "/srv/s3"
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # You can copy, modify, distribute and perform the work, even for
 # commercial purposes, all without asking permission.
 #
-directory "/srv/s3" do
+directory node["fake-s3"]["s3root"] do
   owner "vagrant"
   group "vagrant"
   mode 0755
@@ -23,16 +23,30 @@ execute "link_fakes3" do
   creates "/usr/local/bin/fakes3"
 end
 
-template "fakes3.upstart.conf" do
-  path "/etc/init/fakes3.conf"
-  source "fakes3.upstart.conf.erb"
+# template "fakes3.upstart.conf" do
+#   path "/etc/init/fakes3.conf"
+#   source "fakes3.upstart.conf.erb"
+#   owner "root"
+#   group "root"
+#   mode 0644
+# end
+
+template "/etc/init.d/fakes3" do
+  # path "/etc/init.d/fakes3"
+  source "fakes3.erb"
   owner "root"
   group "root"
-  mode 0644
+  mode 0755
+  variables({
+              "s3root"        => node["fake-s3"]["s3root"],
+              "s3port"        => node["fake-s3"]["s3port"],
+            })
+  # notifies :restart, 'service[fakes3]'
 end
 
 service "fakes3" do
-  provider Chef::Provider::Service::Upstart
-  supports :restart => true, :start => true, :stop => true
+  supports :restart => true
   action [:enable, :start]
 end
+
+
